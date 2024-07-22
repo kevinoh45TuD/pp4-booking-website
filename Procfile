6e58f0350be5966310c40f4df6328c46d0148600
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2024-07-22 03:02:12 +0100

    Add: Heroku Setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn classics_project.wsgi
--- classics_project/settings.py
+++ classics_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ojz=+s^_w+p@4+a7b1z&7u35h(a=76ly0#9z9^475fhe%e#y8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pp4-classics-cuisine.herokuapp.com']
 
 
 # Application definition
