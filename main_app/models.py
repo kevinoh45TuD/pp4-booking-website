c0b5b83f8ab0737c2fce20de3afdf6b71642b1d7
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2024-07-22 04:02:24 +0100

    Add: Database model for bookings

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+DAYS = (
+    (1, "Monday"),
+    (2, "Tuesday"),
+    (3, "Wednesday"),
+    (4, "Thursday"),
+    (5, "Friday"),
+    (6, "Saturday"),
+    (7, "Friday")
+)
+
+TABLE = ((1, "1"),(2, "2"),(3, "3"),(4, "4"),(5, "5"))
 
 # Create your models here.
+class Booking(models.Model):
+    which_day = models.IntegerField(choices=DAYS)
+    which_table = models.IntegerField(choices=TABLE)
+    which_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="curr_book")
