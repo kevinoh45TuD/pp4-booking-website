4916b473e28171538da1de00b62df9a614fc37b8
Kevin <61233166+kevinoh45TuD@users.noreply.github.com>
2024-07-22 04:27:27 +0100

    Edit: Database model for movies

--- main_app/models.py
+++ main_app/models.py
@@ -13,8 +13,31 @@ DAYS = (
 
 TABLE = ((1, "1"),(2, "2"),(3, "3"),(4, "4"),(5, "5"))
 
+MOVIES = (
+    (1, "The Good, The Bad and the Ugly"),
+    (2, "Planet of the Apes"),
+    (3, "Enter the Dragon"),
+    (4, "Escape from Alcatraz"),
+    (5, "The Godfather"),
+    (6, "Night of the Living Dead"),
+    (7, "Monty Python and the Holy Grail")
+)
+
+AVAIL = ((0, "No"), (1, "Yes"))
+
 # Create your models here.
 class Booking(models.Model):
     which_day = models.IntegerField(choices=DAYS)
     which_table = models.IntegerField(choices=TABLE)
     which_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="curr_book")
+
+class Movies(models.Model):
+    pick_day = models.IntegerField(choices=DAYS)
+    table_one = models.IntegerField(choices=AVAIL)
+    table_two = models.IntegerField(choices=AVAIL)
+    table_three = models.IntegerField(choices=AVAIL)
+    table_four = models.IntegerField(choices=AVAIL)
+    table_five = models.IntegerField(choices=AVAIL)
+    which_movie = models.IntegerField(choices=MOVIES)
+    which_year = models.IntegerField()
+    which_cuisine = models.CharField()
